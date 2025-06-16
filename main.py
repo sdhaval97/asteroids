efec74a2b176f37a00e1f19a17ea661caebb1447
sdhaval97 <sdhaval97@gmail.com>
2025-06-16 14:56:11 +0000

    created a sceen using pygame

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 
 def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	number = 5
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			return
+	
+	while number > 0:
+		for event in pygame.event.get():
+  			 if event.type == pygame.QUIT:
+        			return
+		screen.fill("black")
+		pygame.display.flip()
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
