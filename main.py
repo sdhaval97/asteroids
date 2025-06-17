83ae3230fb28d64e37e50ea8573081f7fe41b057
sdhaval97 <sdhaval97@gmail.com>
2025-06-17 13:14:59 +0000

    Updated the main code

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	
+	clock = pygame.time.Clock()
+	dt = 0
+	
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
 	while True:
 		for event in pygame.event.get():
   			 if event.type == pygame.QUIT:
@@ -12,11 +19,8 @@ def main():
 
 		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
 		
-		print("Starting Asteroids!")
-		print(f"Screen width: {SCREEN_WIDTH}")
-		print(f"Screen height: {SCREEN_HEIGHT}")
-
 if __name__ == "__main__":
 	main()
 
