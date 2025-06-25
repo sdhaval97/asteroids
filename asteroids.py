025d34ad9c7e9071659fa9e35ed4667425ff748d
sdhaval97 <sdhaval97@gmail.com>
2025-06-25 17:28:10 +0000

    added the split method in asteroid class

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+import random
 
 class Asteroid(CircleShape, pygame.sprite.Sprite):
     containers = ()
@@ -14,4 +15,24 @@ class Asteroid(CircleShape, pygame.sprite.Sprite):
         
     def update(self, dt):
         self.position += self.velocity * dt
+        
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        
+        vel1 = self.velocity.rotate(random_angle)
+        vel2 = self.velocity.rotate(-random_angle)  
+        
+        new_radius = self.radius * ASTEROID_MIN_RADIUS
+        
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        a1.velocity = vel1 * 1.2
+        a2.velocity = vel2 * 1.2
+        
         
