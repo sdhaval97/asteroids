03254ebb44c71a619013ba4ba9df0cb07f24724a
sdhaval97 <sdhaval97@gmail.com>
2025-06-28 07:05:18 +0000

    added the bgm code in the main file

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from scoreboard import ScoreBoard
 
 def main():
 	pygame.init()
+	pygame.mixer.init()
+	pygame.mixer.music.load("space_theme.ogg")
+	pygame.mixer.music.set_volume(0.5)
+	pygame.mixer.music.play(-1)
+	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	
 	clock = pygame.time.Clock()
